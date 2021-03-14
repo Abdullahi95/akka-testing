b7c430c093a0f8e0e11e98a9755aa9584cb2ff9e
Hassan822 <Hassan931221@gmail.com>
2021-03-14 18:17:04 +0000

    Writing Direct Tests
    You can think of direct testing as a form of white box testing and what that basically means is that we are testing with knowledge of the internals, so we can imagine a transparent box, for example where we can see the internals of the machinery.

---
+++ ActorModel/ActorModel.Tests/StatisticsActorTests.cs
@@ -0,0 +1,42 @@
+﻿using ActorModel.Actors;
+using Akka.TestKit.Xunit2;
+using System.Collections.Generic;
+using Xunit;
+using ActorModel.Messages;
+using System.Collections.ObjectModel;
+
+namespace ActorModel.Tests
+{
+    public class StatisticsActorTests : TestKit
+    {
+
+        [Fact]
+        public void ShouldHaveInitialPlayCountsValue()
+        {
+            // arrange
+            StatisticsActor statisticsActor = new StatisticsActor();
+
+            // act
+            var expected = statisticsActor.PlayCounts;
+
+            // assert
+            Assert.Null(expected);
+        }
+
+        [Fact]
+        public void ShouldSetInitialPlayCounts()
+        {
+            // arrange
+            StatisticsActor statisticsActor = new StatisticsActor();
+
+            // act
+            var movies = new Dictionary<string, int>() { { "Locked Down", 3 }, { "Outside the Wire", 1 } };
+            statisticsActor.HandleInitialStatisticsMessage(new InitialStatisticsMessage(new ReadOnlyDictionary<string, int>(movies)));
+
+            // assert
+            Assert.Equal(3, statisticsActor.PlayCounts["Locked Down"]);
+            Assert.Equal(1, statisticsActor.PlayCounts["Outside the Wire"]);
+        }
+
+    }
+}
---
+++ ActorModel/ActorModel/Actors/StatisticsActor.cs
@@ -0,0 +1,22 @@
+﻿using ActorModel.Messages;
+using Akka.Actor;
+using System.Collections.Generic;
+
+namespace ActorModel.Actors
+{
+    public class StatisticsActor : ReceiveActor
+    {
+        public Dictionary<string, int> PlayCounts { get; set; }
+
+        public StatisticsActor()
+        {
+            this.Receive<InitialStatisticsMessage>(message => HandleInitialStatisticsMessage(message));
+        }
+
+        public void HandleInitialStatisticsMessage(InitialStatisticsMessage message)
+        {
+            this.PlayCounts = new Dictionary<string, int>(message.PlayCounts);
+        }
+
+    }
+}
